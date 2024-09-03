e3070367262a5d0752a40814edc5a4acfb516297
Rahul Negi <rsnegi6923@gmail.com>
2024-09-03 11:54:51 +0530

    complite variables in js

--- 01_Variable.js
+++ 01_Variable.js
@@ -1,2 +1,106 @@
-console.log("hii my name is rahul Negi");
+// let Fname = "Rahul";
+// let surname = "Negi";
+
+// let full_name = Fname + " " + surname;
+
+// console.log(full_name);
+
+// let age = 19;
+
+// console.log(age);
+
+//  age = 35;
+
+// console.log(age);
+
+// const pi =3.14;
+
+// console.log(pi);
+
+// pi=33;
+
+// console.log((3+2)-76*(1+1));
+
+// let a;
+
+// // console.log(9*a);
+
+// let b = 7 * a;
+
+// console.log(b);
+
+// const max ?per);
+
+
+// let  x = 100/(50*3);
+
+// console.log(x);
+
+// for(var i =0 ;i<3;i++){
+    
+//         console.log(i);
+        
+    
+// }
+
+
+// function exam(){
+//     let a =1;
+//     if(true){
+//         let a =2;
+//         console.log(a);
+        
+//     }
+//     console.log(a);
+    
+// }
+// exam();
+
+// var x = 10;
+// function foo() {
+//   var x = 20;
+//   function bar() {
+//     console.log(x);
+//   }
+//   bar();
+// }
+// foo();
+
+// const obj = { name: 'Alice' };
+// obj.name = 'Bob';
+// console.log(obj.name);
+// // obj = { name: 'Charlie' };
+// // console.log(obj.name);
+
+// function test() {
+//     console.log(a);
+//     if (true) {
+//       var a = 5;
+//     }
+//     console.log(a);
+//   }
+//   test();
+
+// show = ()=>{
+//     let x = 10;
+//     const y = 20;
+//     console.log(x);
+//     console.log(y);
+//   }
+//   show();
+//   console.log(x);
+//   console.log(y);
+  
+//   sum = (x,y)=>{
+//     let add = x+y;
+//     console.log(add);
+    
+//   }
+ 
+  
+//   sum(10,10);
+
+// console.log(x);
+// var x = 5;
+
 
