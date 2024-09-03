4e5709512d3b8f817c04782f35c695bd2453480d
Rahul Negi <rsnegi6923@gmail.com>
2024-09-03 23:21:18 +0530

    done some conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,13 @@
+let age = "20"
+
+// console.log(typeof(age));
+
+let valueInNumber = Number(age)
+// console.log(typeof(valueInNumber));
+
+let num = 10n
+
+let bitint_inNumber = Number (num)
+
+// console.log(typeof bitint_inNumber);
+// console.log( bitint_inNumber);
