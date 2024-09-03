e85dc2001d2d83c5354d6c75654c32ebdfdeadff
Rahul Negi <rsnegi6923@gmail.com>
2024-09-03 23:19:57 +0530

    Note all the data type but this file is not complited

---
+++ 02_DataType.js
@@ -0,0 +1,33 @@
+// Data Types in JS
+
+// Primitive
+
+// 1. Number  // 20
+// 2. String  // "Rahul"
+// 3. Boolean // True / False
+// 4. Null    
+// 5. Undefined
+// 6. BigInt
+// 7. Sysmbols // unique identifire for objects
+// 8. typeof // to check the type of any datatype
+
+// 1 Object // special type of datatype
+
+
+  let text = "Rahul_Singh_Negi";
+  let length = text.length;
+
+  console.log(length);
+
+  let text1 = "Rahul_Singh_Negi";
+  let char = text.charAt(0);
+
+  console.log(char);
+
+  let text2 = "Rahul_Singh_Negi";
+  let charValue = text.charCodeAt(0);
+
+  console.log(charValue);
+  
+  
+  
