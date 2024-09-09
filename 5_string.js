8bdd7fe5c55c249561bf2b5e9311c7e5daf33436
Rahul Negi <rsnegi6923@gmail.com>
2024-09-09 22:51:36 +0530

    string methods

--- 5_string.js
+++ 5_string.js
@@ -10,7 +10,8 @@ let fname = "rahul";
 
 // console.log(`my name is ${fname + " " + lname}`);
 
-// const gameName = new String('rahul')
+// 
+
 // console.log(fname.toUpperCase());
 // console.log(fname.length);
 // console.log(fname.indexOf("l"));
@@ -18,9 +19,32 @@ let fname = "rahul";
 // console.log(fname.charAt(3));
 
 const Name = fname.substring(0,4)
-console.log(Name);
-console.log(fname);
-console.log(fname.slice());
+// console.log(Name);
+// console.log(fname);
+// console.log(fname.slice());
+
+const myname = 'Rahul_Singh_Negi';
+console.log(myname);
+
+// console.log(myname.charAt(1));
+// console.log(myname.charCodeAt('R'));
+// console.log(myname.concat());
+//    console.log(myname.constructor());
+//   console.log(myname.endsWith());
+//   console.log(myname.fromCharcode());
+// console.log(myname.includes());
+// console.log(myname.indexOf('l'));
+// console.log(myname.match());
+// console.log(myname.prototype());
+// console.log(myname.repeat());
+// console.log(myname.replace());
+// console.log(myname.search());
+// console.log(myname.split('_'));
+// console.log(myname.slice());
+
+
+
+
 
 
 
