baa96e71cff665cf5cb5083ff78a9795888d7abc
Rahul Negi <rsnegi6923@gmail.com>
2024-09-13 21:53:46 +0530

    add array file

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,27 @@
+const arr = [1, 2, 3, 4, 5];
+// console.log(arr[1]);
+
+// ******** METHODS ********
+
+// 1
+
+// arr.push(7, 8, 6);
+// arr.pop();
+// arr.pop();
+// arr.pop();
+
+
+
+console.log("A ", arr);
+
+const newarr1 = arr.slice(0,3)
+console.log(newarr1);
+
+console.log("B", arr);
+
+const newarr2 = arr.splice(0,3)
+console.log("C",arr);
+
+console.log( newarr2);
+
+
