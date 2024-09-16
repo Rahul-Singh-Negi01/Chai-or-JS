46a806e602265fc76a36911f5be2ecf6e770d5a2
Rahul Negi <rsnegi6923@gmail.com>
2024-09-16 22:21:35 +0530

    some new concept in loops

--- 02_Basic/01_array.js
+++ 02_Basic/01_array.js
@@ -12,16 +12,37 @@ const arr = [1, 2, 3, 4, 5];
 
 
 
-console.log("A ", arr);
+// console.log("A ", arr);
 
 const newarr1 = arr.slice(0,3)
-console.log(newarr1);
+// console.log(newarr1);
 
-console.log("B", arr);
+// console.log("B", arr);
 
 const newarr2 = arr.splice(0,3)
-console.log("C",arr);
+// console.log("C",arr);
 
-console.log( newarr2);
+// console.log( newarr2);
+
+const cars = [1,2,3,4,5]
+let car = cars[4]
+// console.log(car);
+
+const fruits = [4,8,3,5,1,2,0];
+// console.log(fruits.sort());
+// console.log(cars.length);
 
 
+/* ******LOOPS******* */
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+console.log(cats);
+
+const cats1 = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (let i = 0; i < cats.length; i++) {
+  console.log(cats1[i]);
+}
