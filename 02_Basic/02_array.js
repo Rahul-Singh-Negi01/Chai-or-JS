ccfdadaa7a2607d6e8db295259f01e9a59bc167a
Rahul Negi <rsnegi6923@gmail.com>
2024-09-21 22:59:02 +0530

    more things in js

--- 02_Basic/02_array.js
+++ 02_Basic/02_array.js
@@ -11,13 +11,13 @@ const arr1 = ["piyush", "vicky", "vimal"];
 // const newarr = arr.concat(arr1);
 // console.log(newarr);
 
-// const new_arr = [...arr, ...arr1]
-// console.log(new_arr);
+const new_arr = [...arr, ...arr1]
+console.log(new_arr);
 
 const another_array = [1,2,3,[3,2,8],4,5,6,[4,5,[2,3]],7,[8,9]]
 const real_another_array = another_array.flat(Infinity)
 
-console.log(real_another_array.sort());
+// console.log(real_another_array.sort());
 
 // Array.isArray
 // Array.from
--- 04_objects.js
+++ 04_objects.js
@@ -17,5 +17,24 @@ const newobject = {
         }
     }
 }
-console.log(newobject.fullname.username.lname);
+// console.log(newobject.fullname.username.lname);
 
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"a",
+    4:"b"
+}
+
+const obj3 = {
+    5:"a",
+    6:"b"
+}
+
+const newobj = {...obj1,...obj2,...obj3}
+const newobj1 = Object.assign({},obj1,obj2,obj3)
+console.log(newobj);
+console.log(newobj1);
