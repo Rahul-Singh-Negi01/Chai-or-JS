99564debd236c80f3fb35fce758513ff5aa78be4
Rahul Negi <rsnegi6923@gmail.com>
2024-09-27 23:04:40 +0530

    objects inside functions

--- 03_Basic/01_Function.js
+++ 03_Basic/01_Function.js
@@ -21,7 +21,7 @@ function add(){
 
 function add1(number1,number2){
    
-    console.log(number1+number2);
+    // console.log(number1+number2);
 
     
 }
@@ -29,17 +29,35 @@ const newadd1 = add1(2,4);
 // add1(2,5)
 // add1(2,'a')
 // add1(2,null)
-console.log(newadd1);
+// console.log(newadd1);
 
 
 function add2(number1,number2){
    
     // console.log(number1+number2);
-    return number1+number2;    
+    // return number1+number2;    
 }
 const newadd2 = add2(2,4);
 // console.log(typeof newadd2);
 // console.log(newadd2);
 
+function calculatePrice(val1, ...num1){
+    return num1
+}
 
+// console.log(calculatePrice(100,200,300,400,500));
+const user = {
+    username:"rahul",
+    price:199
+}
 
+function handleobj(anyobject){
+    // console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleobj(user);
+handleobj({
+    username:"negi",
+    price:1000000
+});
