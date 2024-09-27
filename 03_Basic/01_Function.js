3317ef9095d541644ce133fa6527e48125a20ae8
Rahul Negi <rsnegi6923@gmail.com>
2024-09-27 22:42:22 +0530

    basic function

---
+++ 03_Basic/01_Function.js
@@ -0,0 +1,45 @@
+function myname() {
+console.log('R');
+console.log('A');
+console.log('H');
+console.log('U');
+console.log('L');
+}
+// myname();
+
+function sum(a,b){
+    return a+b;
+}
+
+// console.log(sum(2,4));
+ 
+function add(){
+    return 1+2;
+}
+
+// console.log(add());
+
+function add1(number1,number2){
+   
+    console.log(number1+number2);
+
+    
+}
+const newadd1 = add1(2,4);
+// add1(2,5)
+// add1(2,'a')
+// add1(2,null)
+console.log(newadd1);
+
+
+function add2(number1,number2){
+   
+    // console.log(number1+number2);
+    return number1+number2;    
+}
+const newadd2 = add2(2,4);
+// console.log(typeof newadd2);
+// console.log(newadd2);
+
+
+
---
+++ 03_Basic/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function add1(number1,number2){
+   
+//     console.log(number1+number2);
+
+    
+// }
+// const newadd1 = add1(2,4);
+// console.log(newadd1);
