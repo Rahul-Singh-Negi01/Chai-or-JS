ce26692bbc5cd53c64eed0e87ac53948d56c3bfe
Rahul Negi <rsnegi6923@gmail.com>
2024-09-28 19:55:47 +0530

    obj

--- 02_Basic/03_objects.js
+++ 02_Basic/03_objects.js
@@ -25,9 +25,28 @@ user.email = "rsn@gmail.com";
 // console.log(user);
 
 user.greeting = function(){
-    console.log(`hiiii, ${this.age}`);
+    // console.log(`hiiii, ${this.age}`);
     
 }
-console.log(user.greeting());
+// console.log(user.greeting());
 
 // user.greeting("rahul")
+// let user = new Object();
+// user.rahul = true
+// console.log(user);
+
+let userdetail = {
+    name:"rahul"
+    ,age:20,
+    caste:"negi"
+}
+// userdetail.mid_name = true
+// userdetail["like birds"] =true
+
+// let key = prompt("What do you want to know about the user?", "name");
+
+// alter(userdetail[key])
+
+// delete userdetail.caste
+// console.log(userdetail);
+
