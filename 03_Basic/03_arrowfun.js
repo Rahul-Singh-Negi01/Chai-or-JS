c758f6936298bd533fecfff31e360518d8dd8684
Rahul Negi <rsnegi6923@gmail.com>
2024-09-29 23:43:08 +0530

    this in objects

--- 03_Basic/03_arrowfun.js
+++ 03_Basic/03_arrowfun.js
@@ -3,4 +3,20 @@ const sum = (a,b)=>{
     console.log(a+b);
 }
 
-sum(2,3)
+// sum(2,3)
+
+const user = {
+    username: "rahul",
+    price:999,
+
+    WelcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);  
+        console.log(this);
+        
+    }
+}
+
+// user.WelcomeMessage()
+// user.username  = "piyush"
+// user.WelcomeMessage()
+console.log(this);
--- 03_Basic/tempCodeRunnerFile.js
+++ 03_Basic/tempCodeRunnerFile.js
@@ -1,2 +1,3 @@
 
-        // console.log(web);   
+user.username  = "piyush"
+user.WelcomeMessage()
