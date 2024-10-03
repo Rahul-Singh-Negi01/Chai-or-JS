78a963b384566d1b63e78d27019e3005eef4487b
Rahul Negi <rsnegi6923@gmail.com>
2024-10-03 23:33:59 +0530

    conditions

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,22 +1,57 @@
 // if
 
-const isuserloggedin = true;
+// const isuserloggedin = true;
 
-if (isuserloggedin) {
-    // console.log("rahul");
-}
+// if (isuserloggedin) {
+//     // console.log("rahul");
+// }
 
-if (3!=2) {
-    // console.log("name");
+// if (3!=2) {
+//     // console.log("name");
     
-}
-if (2 !== "2") {
-    console.log("true");
+// }
+// if (2 !== "2") {
+//     console.log("true");
     
-}
-else{
-    console.log("flase");
+// }
+// else{
+//     console.log("flase");
     
-}
+// }
 
 // <, >, <=, >=, ==, ===, !=
+
+// const score = 200
+
+// if (score>100) {
+//     const power = "fly";
+    // var power = "fly";
+    // console.log(`User Power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+if(true) {
+    // console.log(123);   
+}
+    
+// // // Nesting // // //
+
+const balance =1000;
+
+if (balance < 500) {
+    
+}
+else if (balance < 750) {
+    console.log("less than 750");
+    
+}
+else if (balance < 800) {
+    console.log("less than 800");
+    
+}
+else {
+    console.log("less than 1000");
+
+}
+    
