eab9d22ece71aec60e79003d26cffc371498fcf0
Rahul Negi <rsnegi6923@gmail.com>
2024-10-03 23:21:46 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+// if
+
+const isuserloggedin = true;
+
+if (isuserloggedin) {
+    // console.log("rahul");
+}
+
+if (3!=2) {
+    // console.log("name");
+    
+}
+if (2 !== "2") {
+    console.log("true");
+    
+}
+else{
+    console.log("flase");
+    
+}
+
+// <, >, <=, >=, ==, ===, !=
